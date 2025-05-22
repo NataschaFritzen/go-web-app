811e12c4e25654e7864f5a49127e95f0a3862a76
Natascha Fritzen <119437922+NataschaFritzen@users.noreply.github.com>
2025-05-22 16:20:22 -0300

    adicionar estrutura basica do servidor e template HTML para listagem de produtos

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq" // Motivo do "_", para utilizar durante o tempo de execução da aplicação
 )
+
+// Insira suas credenciais de acesso ao banco de dados
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user dbname password host sslmode"
+
+}
+
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
@@ -13,5 +29,12 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tenis", "Confortável", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Mouse", "Rápido", 15, 3},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
